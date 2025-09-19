c3857d1d262db4e4f403821cb4a4d55a99e396fd
javila1996 <javila2@byupathway.edu>
2025-09-19 01:04:05 -0600

    fraction assignment week 3

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine("Fraction1 (default): " + fraction1.GetFractionString() +
+                          " = " + fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(6);
+        Console.WriteLine("Fraction2 (6/1): " + fraction2.GetFractionString() +
+                          " = " + fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(6, 7);
+        Console.WriteLine("Fraction3 (6/7): " + fraction3.GetFractionString() +
+                          " = " + fraction3.GetDecimalValue());
+
+        fraction1.SetNumerator(3);
+        fraction1.SetDenominator(4);
+        Console.WriteLine("Fraction1 after update: " + fraction1.GetFractionString() +
+                          " = " + fraction1.GetDecimalValue());
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine("Fraction4 (1/3): " + fraction4.GetFractionString() +
+                          " = " + fraction4.GetDecimalValue());
     }
 }
